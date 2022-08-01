1d0936bd42bcc39e3bb2384a90be43b7e05ad527
Abijith-s <abijithsebastian66@gmail.com>
2022-08-01 14:51:52 +0530

    file structure added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+
+app.get('/login',(req,res)=>{
+  
+})
 app.listen(port, () => {
   console.log(`Success! Your application is running on port ${port}.`);
 });
